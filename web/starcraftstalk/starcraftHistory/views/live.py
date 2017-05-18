d71a2b8d43fae1be20f85b2e76c6750cb81487d1
Nicolas <nicolas.ninin@gmail.com>
2017-05-19 00:38:01 +0200

    live

--- web/starcraftstalk/starcraftHistory/views/live.py
+++ web/starcraftstalk/starcraftHistory/views/live.py
@@ -1,6 +1,6 @@
 from ..models import League,Games,Global
 from django.shortcuts import render
-import time
+import time,datetime
 import json
 from django.http import HttpResponse,JsonResponse
 from django.core import serializers
@@ -12,9 +12,8 @@ def RepresentsInt(s):
         return False
 
 def gamesDict(date):
-    print(date)
     return  Games.objects.filter(
-        	pk__gt=date).select_related(
+        	pk__gt=date,date__isnull=False).select_related(
         	"player").order_by(
         	"-date").values(
         	"date","guessopgameid__current_mmr","guessopgameid__guessmmrchange",
@@ -22,6 +21,21 @@ def gamesDict(date):
         	"guessopgameid__path","guessopid__smurf__pseudo","guessopgameid__player",
         	"guessopid__mainrace","player__mainrace","pk"
         	)
+def addHumanDate(games2,timetoadd=0):
+    if type(games2)==dict:
+        games=games2["games"]
+    else:
+        games=games2
+    for g in games:
+        g["date_human"]=datetime.datetime.fromtimestamp(
+        g["date"]).strftime('%d %b %H:%M')
+
+        if g["guessopid__smurf__pseudo"]!= None:
+            g["guessopid__name"]=g["guessopid__smurf__pseudo"]
+        if g["guessopid__name"]==None:
+            g["guessopid__name"]=g["guessopgameid__path"]
+
+    return games
 def last20games():
 
     return  Games.objects.all().select_related(
@@ -35,16 +49,18 @@ def last20games():
 def recentlive(request):
     maxpk=Games.objects.latest("pk").pk
     games=gamesDict(maxpk-10)
+    games=addHumanDate(games)
     context={"time":time.time(),"games":games,"server":"","maxpk":maxpk-5}
     return render(request,'starcraftHistory/testlive.html',context)
 def lastmatchsince(request):
     print("azeazeze")
     print(request.GET)
-    print(request.POST)
-    print(request.GET["date"])
     if RepresentsInt(request.GET["date"]):
         date=int(request.GET["date"])
-        games={"games":list(gamesDict(date))}
+        games=gamesDict(date)
+        games=addHumanDate(games)
+        games={"games":list(games)}
+        print(games)
         #print((games))
     else:
         games={}
