801eab6f1575a725129e4cb33c6ab14b832a8258
Nicolas <nicolas.ninin@gmail.com>
2017-05-29 00:35:26 +0200

    hack undone

--- web/starcraftstalk/starcraftHistory/views/wcs.py
+++ web/starcraftstalk/starcraftHistory/views/wcs.py
@@ -112,7 +112,7 @@ def wcs(request,server):
 
 	num=1
 	listegoodplayerid=[]
-	gamesbetween=Games.objects.filter(server=server,date__range=(start-86400,end),
+	gamesbetween=Games.objects.filter(server=server,date__range=(start,end),
 	player__wcs=1,type="SOLO").exclude(ranked="unrank")
 	for p in playerwcs:
 		if p["idplayer"]==1945:
