cf073fd1c9132ffbcc4077337be7060dd32ac2fb
Nicolas <nicolas.ninin@gmail.com>
2017-05-28 15:57:17 +0200

    hour last

--- web/starcraftstalk/starcraftHistory/views/wcs.py
+++ web/starcraftstalk/starcraftHistory/views/wcs.py
@@ -92,7 +92,7 @@ def wcs(request,server):
 		thresh=6300
 		startdate=datetime.datetime(2017,5,25,21)
 		getDates(startdate,"eu")
-		lastday=datetime.datetime(2017,5,28,23,59)
+		lastday=datetime.datetime(2017,5,28,21,59)
 
 
 	(start,end)=getPromotionWindows(server)
